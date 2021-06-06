82ce2f0df83ad0e52ac5f047fb54f691f24b54ca
lukas <lukas.faatz@codecamp-n.com>
2021-06-06 10:25:32 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'customDirective';
+  hasErrors = true;
+
+  name = new FormControl('', Validators.required);
+
 }
